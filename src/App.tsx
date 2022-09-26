f5235c5b6d6f972d7a82ed7e92c151e5faa96a4c
julia snegova <ssnnggvv@gmail.com>
2022-09-26 14:35:53 +0300

    products data is added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Container } from "react-bootstrap";
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import { NavBar } from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <NavBar/>
+      <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/store" element={<Store/>}></Route>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+
 export const Store = () => {
-    return <h1>store</h1>
-}
+  const [productData, setProductData] = useState([] as any[]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.storerestapi.com/products");
+        const json = await response.json();
+        setProductData(json.data);
+        console.log(json.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {productData.map((product) => (
+          <li key={product._id}>
+            {product.title}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
